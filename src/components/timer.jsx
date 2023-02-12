c31e64b8e7c33ae17c26f014e0a2501ccebf741a
Laraibi <meh.laraibi@gmail.com>
2023-02-12 13:22:58 +0100

    fix timer bug

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -2,17 +2,35 @@ import { useState, useEffect } from "react";
 
 const Timer = () => {
   const [seconds, setseconds] = useState(0);
-  const startTimer = () =>
-    setInterval(() => {
-      setseconds(seconds + 1);
-      console.log("setInterval");
-    }, 1000);
-  //   startTimer();
   useEffect(() => {
-    startTimer;
+    const int = setInterval(() => {
+      setseconds((seconds) => seconds + 1);
+      console.log(seconds)
+    }, 1000);
+    return () => clearInterval(int);
   }, []);
 
-  return <div className="ps">{seconds}</div>;
+  return <div className="">{seconds}</div>;
 };
 
 export default Timer;
+// import React, { useState, useEffect } from 'react';
+
+// function Timer() {
+//   const [seconds, setSeconds] = useState(0);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setSeconds(seconds => seconds + 1);
+//     }, 1000);
+//     // return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <div>
+//       Seconds: {seconds}
+//     </div>
+//   );
+// }
+
+// export default Timer;
